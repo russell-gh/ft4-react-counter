f52b27eee4b087414ecf22c30e2f2ba020a6627a
russell <russell>
2023-10-24 13:44:45 +0100

    react todos list with api

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Footer extends Component {
-  render() {
-    return <footer>Copyright 2023</footer>;
-  }
-}
-
-export default Footer;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Spinner extends Component {
+  render() {
+    return (
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Spinner;
